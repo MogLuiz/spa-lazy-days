fba23dabecd8559e08265eca92dbeb08495d1ab9
Luiz Henrique <luizhjramos@outlook.com>
2022-06-16 15:57:31 -0300

    chore(eslint): rules disable

--- client/src/components/treatments/Treatments.tsx
+++ client/src/components/treatments/Treatments.tsx
@@ -13,7 +13,7 @@ export function Treatments(): ReactElement {
         Available Treatments
       </Heading>
       <HStack m={10} spacing={8} justify="center">
-        {treatments.map((treatmentData) => (
+        {treatments?.map((treatmentData) => (
           <Treatment key={treatmentData.id} treatmentData={treatmentData} />
         ))}
       </HStack>

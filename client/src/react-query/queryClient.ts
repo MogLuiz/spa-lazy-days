17382cd5e48b85d24fb8d5b608aa51e5f8e93f8d
Luiz Henrique <luizhjramos@outlook.com>
2022-06-17 22:38:07 -0300

    feat(app): global queryClient error handling with react-query

--- client/src/react-query/queryClient.ts
+++ client/src/react-query/queryClient.ts
@@ -1,20 +1,26 @@
 import { QueryClient } from 'react-query';
-// import { createStandaloneToast } from '@chakra-ui/react';
-// import { theme } from '../theme';
+import { createStandaloneToast } from '@chakra-ui/react';
+import { theme } from '../theme';
 
-// const toast = createStandaloneToast({ theme });
+const toast = createStandaloneToast({ theme });
 
-// function queryErrorHandler(error: unknown): void {
-//   // error is type unknown because in js, anything can be an error (e.g. throw(5))
-//   const id = 'react-query-error';
-//   const title =
-//     error instanceof Error ? error.message : 'error connecting to server';
+function queryErrorHandler(error: unknown): void {
+  // error is type unknown because in js, anything can be an error (e.g. throw(5))
+  const id = 'react-query-error';
+  const title =
+    error instanceof Error ? error.message : 'error connecting to server';
 
-//   // prevent duplicate toasts
-//   toast.closeAll();
-//   toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
-// }
+  // prevent duplicate toasts
+  toast.closeAll();
+  toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
+}
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      onError: queryErrorHandler,
+    },
+  },
+});
 
 export default queryClient;

70c2a238dfcbceb563c584c1fc4ac44dc4591c35
Luiz Henrique <luizhjramos@outlook.com>
2022-06-17 21:54:58 -0300

    feat(treatments): useTreatments error handling in the custom hook

--- client/src/components/treatments/hooks/useTreatments.ts
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -11,7 +11,17 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
+  const toast = useCustomToast();
+
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys['treatments'], getTreatments);
+  const { data = fallback } = useQuery(queryKeys['treatments'], getTreatments, {
+    onError: (error) => {
+      const title =
+        error instanceof Error
+          ? error.message
+          : 'error connecting to the server';
+      toast({ title, status: 'error' });
+    },
+  });
   return data;
 }
